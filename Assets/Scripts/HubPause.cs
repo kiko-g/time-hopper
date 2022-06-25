894e9a2b6af9f257709e823477e34bafde2c9921
kiko-g <kikojpgoncalves@gmail.com>
2022-06-25 16:08:34 +0100

    edited computer hub menu and add pause menu component

--- Assets/Scripts/HubPause.cs
+++ Assets/Scripts/HubPause.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class HubComputer : MonoBehaviour
+public class HubPause : MonoBehaviour
 {
     public Canvas canvas;
 
