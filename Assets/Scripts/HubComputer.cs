756623e24088c036ceb478d1b7fb9afe37491aed
kiko-g <kikojpgoncalves@gmail.com>
2022-06-28 16:03:30 +0100

    add: toggle computer menus

--- Assets/Scripts/HubComputer.cs
+++ Assets/Scripts/HubComputer.cs
@@ -25,7 +25,7 @@ public class HubComputer : MonoBehaviour
         canvas = GetComponent<Canvas>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<StarterAssets.ThirdPersonController>();
         pauseCanvas = GameObject.FindGameObjectWithTag("PauseHUD").GetComponent<Canvas>();
-        
+
         arenasButton.onClick.AddListener(OnClickArenas);
         overviewButton.onClick.AddListener(OnClickOverview);
         upgradesButton.onClick.AddListener(OnClickUpgrades);
@@ -84,8 +84,8 @@ public class HubComputer : MonoBehaviour
 
     bool IsWithinRange(Vector3 pos)
     {
-        float xMaxLeft = -10.0f, xMinLeft = -16.0f, zMaxLeft = -6.0f, zMinLeft = -9.0f;
-        float xMaxRight = -0.5f, xMinRight = -6.0f, zMaxRight = -6.0f, zMinRight = -9.0f;
+        float xMaxLeft = -9f, xMinLeft = -16.0f, zMaxLeft = -5.0f, zMinLeft = -9.0f;
+        float xMaxRight = 0.0f, xMinRight = -7.5f, zMaxRight = -5.0f, zMinRight = -9.0f;
 
         bool left = (pos.x < xMaxLeft && pos.x > xMinLeft && pos.z < zMaxLeft && pos.z > zMinLeft);
         bool right = (pos.x < xMaxRight && pos.x > xMinRight && pos.z < zMaxRight && pos.z > zMinRight);
@@ -116,6 +116,10 @@ public class HubComputer : MonoBehaviour
         overviewButtonActive.SetActive(true);
         arenasButtonActive.SetActive(false);
         upgradesButtonActive.SetActive(false);
+
+        overviewCore.SetActive(true);
+        arenasCore.SetActive(false);
+        upgradesCore.SetActive(false);
     }
 
     void OnClickArenas()
@@ -124,6 +128,10 @@ public class HubComputer : MonoBehaviour
         overviewButtonActive.SetActive(false);
         arenasButtonActive.SetActive(true);
         upgradesButtonActive.SetActive(false);
+
+        overviewCore.SetActive(false);
+        arenasCore.SetActive(true);
+        upgradesCore.SetActive(false);
     }
 
     void OnClickUpgrades()
@@ -132,5 +140,9 @@ public class HubComputer : MonoBehaviour
         arenasButtonActive.SetActive(false);
         overviewButtonActive.SetActive(false);
         upgradesButtonActive.SetActive(true);
+
+        overviewCore.SetActive(false);
+        arenasCore.SetActive(false);
+        upgradesCore.SetActive(true);
     }
 }
