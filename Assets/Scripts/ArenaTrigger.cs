3571e9e3c3d711d502eea858ee3bf104ee566bb0
up201806302 <up201806302@fe.up.pt>
2022-07-01 04:17:18 +0100

    Added extraction status

--- Assets/Scripts/ArenaTrigger.cs
+++ Assets/Scripts/ArenaTrigger.cs
@@ -13,24 +13,40 @@ public class ArenaTrigger : MonoBehaviour
     public void performAction(bool death = false)
     {
         bool reduceCurrencies = false;
+        string ExtractionStatus = "";
         if(death){
             if(waveSpawner != null){
                 reduceCurrencies = true;
+                ExtractionStatus = "Extraction sequence completed with errors. Half of the currencies have been lost.";
             }
+            
         }
+    	else{
+            ExtractionStatus = "Successfully completed extraction sequence. All currencies have been extracted.";
+        }
+        ExtractionStatus += "\nColloseum Currency: ";
         if(player.colCurrency >= 0){
             if(reduceCurrencies) player.colCurrency = Mathf.RoundToInt(player.colCurrency * 0.5f);
+            ExtractionStatus += player.colCurrency;
             PlayerPrefs.SetInt("ColliseumCurrency", PlayerPrefs.GetInt("ColliseumCurrency") + player.colCurrency);
         }
+        else ExtractionStatus += "0";
+        ExtractionStatus += "\nFactory Currency: ";
         if(player.forCurrency >= 0){
             if(reduceCurrencies) player.forCurrency = Mathf.RoundToInt(player.forCurrency * 0.5f);
             PlayerPrefs.SetInt("ForestCurrency", PlayerPrefs.GetInt("ForestCurrency") + player.forCurrency);
         }
+        else ExtractionStatus += "0";
+        ExtractionStatus += "\nForest Currency: ";
         if(player.facCurrency >= 0){
             if(reduceCurrencies) player.facCurrency = Mathf.RoundToInt(player.facCurrency * 0.5f);
+            ExtractionStatus += player.facCurrency;
             PlayerPrefs.SetInt("FactoryCurrency", PlayerPrefs.GetInt("FactoryCurrency") + player.facCurrency);
         }
+        else ExtractionStatus += "0";
+        ExtractionStatus += "\nRumble Currency: ";
         if(player.rumCurrency >= 0){
+            ExtractionStatus += player.rumCurrency;
             PlayerPrefs.SetInt("RumbleCurrency", PlayerPrefs.GetInt("RumbleCurrency") + player.rumCurrency);
         }
         if(waveSpawner == null){
@@ -70,7 +86,12 @@ public class ArenaTrigger : MonoBehaviour
                 int currentTimePlayedNew = PlayerPrefs.GetInt("ColliseumTimePlayed");
             }
         }
-
+        if(death && ExtractionStatus != ""){
+            PlayerPrefs.SetString("ExtractionStatus", ExtractionStatus);
+        }
+        else{
+            PlayerPrefs.SetString("ExtractionStatus", "");
+        }
         string arenaAttempts = sceneSwitch.ArenaName+"Attempts";
         PlayerPrefs.SetInt(arenaAttempts, PlayerPrefs.GetInt(arenaAttempts) + 1);
         PlayerPrefs.Save();
